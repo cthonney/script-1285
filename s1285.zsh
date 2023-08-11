04ffaf576fb3f05440b1d6861b55e04d4c6ef9db
Cédric Thonney <82598040+cthonney@users.noreply.github.com>
2023-08-11 14:58:23 +0400

    Update s1285.zsh

--- s1285.zsh
+++ s1285.zsh
@@ -1,27 +1,8 @@
 #!/usr/bin/env zsh
 
-# Arrêter le script si l'une des commandes échoue
-set -e
-
-# Fonction pour gérer l'échec
-handle_error() {
-  echo "Une erreur est survenue. Vérifiez les commandes et réessayez."
-}
-
-# Appeler la fonction si une commande échoue
-trap 'handle_error' ERR
-
 # Récupérer le nom d'utilisateur GitHub
 export GITHUB_USERNAME=`gh api user | jq -r '.login'`
-echo "Le nom d'utilisateur GitHub récupéré est : $GITHUB_USERNAME. Est-ce correct ? (y/n)"
-read confirmation
 
-if [[ "$confirmation" != "y" ]]; then
-  echo "Veuillez entrer votre nom d'utilisateur GitHub :"
-  read GITHUB_USERNAME
-fi
-
-# Les commandes suivantes
 cd ~/code/$GITHUB_USERNAME
 cp -r fullstack-challenges wrong-fullstack-challenges
 cd fullstack-challenges
