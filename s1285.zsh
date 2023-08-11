a477b7b7ef1a75b048818ec64d6dfc9eea36229b
Cédric Thonney <82598040+cthonney@users.noreply.github.com>
2023-08-11 14:52:03 +0400

    Update s1285.zsh

--- s1285.zsh
+++ s1285.zsh
@@ -1,45 +1,46 @@
 #!/usr/bin/env zsh
 
-# Récupérer le nom d'utilisateur GitHub en utilisant l'outil de ligne de commande GitHub
-export GITHUB_USERNAME=$(gh api user | jq -r '.login')
+# Arrêter le script si l'une des commandes échoue
+set -e
 
-# Demander à l'utilisateur de confirmer le nom d'utilisateur récupéré
+# Fonction pour gérer l'échec
+handle_error() {
+  echo "Une erreur est survenue. Vérifiez les commandes et réessayez."
+}
+
+# Appeler la fonction si une commande échoue
+trap 'handle_error' ERR
+
+# Récupérer le nom d'utilisateur GitHub
+export GITHUB_USERNAME=`gh api user | jq -r '.login'`
 echo "Le nom d'utilisateur GitHub récupéré est : $GITHUB_USERNAME. Est-ce correct ? (y/n)"
-read -r confirmation
+read confirmation
 
-if [ "$confirmation" != "y" ]; then
+if [[ "$confirmation" != "y" ]]; then
   echo "Veuillez entrer votre nom d'utilisateur GitHub :"
-  read -r GITHUB_USERNAME
+  read GITHUB_USERNAME
 fi
 
-directory_path=~/code/"$GITHUB_USERNAME"
-
-# Vérifier si le chemin existe
-if [ ! -d "$directory_path" ]; then
-  echo "Le chemin $directory_path n'existe pas. Vérifiez votre nom d'utilisateur GitHub ou le chemin."
+# Vérifier que le dossier fullstack-challenges existe
+if [ ! -d "~/code/$GITHUB_USERNAME/fullstack-challenges" ]; then
+  echo "Le dossier 'fullstack-challenges' n'a pas été trouvé. Assurez-vous d'être dans le répertoire correct et réessayez."
   exit 1
 fi
 
-# Se déplacer dans le répertoire un niveau au-dessus de 'fullstack-challenges' et faire une copie du répertoire
-cd "$directory_path" || exit
+# Les commandes suivantes
+cd ~/code/$GITHUB_USERNAME
 cp -r fullstack-challenges wrong-fullstack-challenges
+cd fullstack-challenges
 
-# Retourner dans le dossier 'fullstack-challenges'
-cd fullstack-challenges || { echo "Le dossier 'fullstack-challenges' n'a pas été trouvé."; exit 1; }
-
-# S'assurer que l'état est propre d'abord
+# Git commandes
 git status
 git add .
-git commit -m "Commit everything before switching to a new branch" || { echo "Commit échoué, vérifiez les changements."; exit 1; }
-
-# Créer une branche pour stocker ce qu'ils ont jusqu'à présent (avec le mauvais contenu) et la pousser sur GitHub
+git commit -m "Commit everything before switch to new branch"
 git checkout -b bootcamp-first-part
-git push origin bootcamp-first-part || { echo "Poussée vers GitHub échouée. Vérifiez vos accès."; exit 1; }
-
-# Revenir à master et forcer l'écrasement de la branche master à la version 'no-update', puis forcer la poussée sur GitHub
+git push origin bootcamp-first-part
 git checkout master
 git fetch upstream
 git reset --hard upstream/no-update
-git push -f origin master || { echo "Poussée forcée échouée. Vérifiez vos accès."; exit 1; }
+git push -f origin master
 
-echo "Opérations terminées avec succès."
+echo "Opérations terminées avec succès!"
